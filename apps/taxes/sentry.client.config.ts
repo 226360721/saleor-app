d02f8fef611d1985efa051bba38060650d4e3ef4
226360721 <95421128+226360721@users.noreply.github.com>
2023-11-07 21:50:14 +0800

    Initial commit

---
+++ apps/taxes/sentry.client.config.ts
@@ -0,0 +1,42 @@
+/*
+ * This file configures the initialization of Sentry on the browser.
+ * The config you add here will be used whenever a page is visited.
+ * https://docs.sentry.io/platforms/javascript/guides/nextjs/
+ */
+
+import * as Sentry from "@sentry/nextjs";
+import { CriticalError } from "./src/error";
+import { shouldExceptionLevelBeReported } from "./src/sentry-utils";
+
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+const SENTRY_ENVIRONMENT = process.env.SENTRY_ENVIRONMENT;
+
+Sentry.init({
+  dsn: SENTRY_DSN,
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1.0,
+  environment: SENTRY_ENVIRONMENT,
+  includeLocalVariables: true,
+  ignoreErrors: ["TRPCClientError"],
+  beforeSend(errorEvent, hint) {
+    const error = hint.originalException;
+
+    if (error instanceof CriticalError) {
+      errorEvent.level = error.sentrySeverity;
+
+      // Ignore exceptions below specified severity (warning default)
+      if (!shouldExceptionLevelBeReported(errorEvent.level ?? "error")) {
+        return null;
+      }
+    }
+
+    return errorEvent;
+  },
+  integrations: [
+    new Sentry.Replay({
+      // Additional Replay configuration goes in here, for example:
+      maskAllText: true,
+      blockAllMedia: true,
+    }),
+  ],
+});
